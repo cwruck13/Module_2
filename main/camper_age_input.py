8fb639581ac41bd0d8cafd7682e7957a8b853021
cwruck13 <66025028+cwruck13@users.noreply.github.com>
2020-06-01 15:07:19 -0500

    Initial commit

---
+++ main/camper_age_input.py
@@ -0,0 +1,10 @@
+"""
+Program: camper_age_input.py
+Author: Cassandra Wruck
+Last date modified: June 1st, 2020
+
+The program will have will prompt for the age of one infant (age 1-5 years)
+who is attending camp and convert the age in months to years via a function call then print the result.
+"""
+
+if __name__ == '__main__'
