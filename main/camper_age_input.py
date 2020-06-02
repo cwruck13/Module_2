5d29e18bd7d3cebe199337de38fae2fe6a8f3141
cwruck13 <66025028+cwruck13@users.noreply.github.com>
2020-06-02 09:29:40 -0500

    Final Commit

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -6,7 +6,18 @@ Last date modified: June 1st, 2020
 The program will have will prompt for the age of one infant (age 1-5 years)
 who is attending camp and convert the age in months to years via a function call then print the result.
 """
-def convert_to_months(x):
-    pass
 
-if __name__ == '__main__'
+# get input from user
+years = input('What is your age?: ')
+
+
+# method to convert years to months
+def convert_to_months(years):
+    years_int = int(years)
+    months = years_int * 12
+    return months
+
+
+# printing return
+if __name__ == '__main__':
+    print(str(convert_to_months(years)) + ' months')
--- test/test_function.py
+++ test/test_function.py
@@ -1,9 +1,20 @@
 import unittest
-from main import camper_age_input
+from main import camper_age_input as cai
 
+#testing method
 class FunctionTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEquals(False, True)
+    def test_12_years_to_months(self):
+        #12 years old should be 144 months old
+        self.assertEqual(144, cai.convert_to_months(12))
+
+    def test_5_years_to_months(self):
+        #5 years old should be 60 months old
+        self.assertEqual(60, cai.convert_to_months(5))
+
+    def test_16_years_to_months(self):
+        #16 years old should be 192 months old
+        self.assertEqual(192, cai.convert_to_months(16))
+
 
 if __name__ == '__main__':
     unittest.main()
